8756715cf9ca4a9107f3f36c3012f1e7a8ad333e
Hugo Cisneros Amengual <159303107+hugocis@users.noreply.github.com>
2024-06-07 21:50:35 +0200

    Deploy Fix

--- src/routes/paymentRoutes.js
+++ src/routes/paymentRoutes.js
@@ -4,8 +4,8 @@ const paymentController = require('../controllers/paymentController');
 const authMiddleware = require('../middleware/authMiddleware');
 
 router.post('/', authMiddleware, paymentController.createPayment);
-router.get('/', authMiddleware, paymentController.getAllPayments);
-router.get('/trip/:trip_id', authMiddleware, paymentController.getPaymentsByTripId);
+router.get('/', authMiddleware, paymentController.getPayments);
+router.get('/trip/:trip_id', authMiddleware, paymentController.getPaymentsByTrip);
 router.delete('/:payment_id', authMiddleware, paymentController.deletePayment);
 
 module.exports = router;
